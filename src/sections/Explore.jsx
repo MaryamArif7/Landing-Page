f221c9793119eb7fa6ec9a70e7b54d69bebbff4b
MaryamArif7 <www.maryamarif28242@gmail.com>
2024-05-12 18:22:25 +0500

    fourth

--- src/sections/Explore.jsx
+++ src/sections/Explore.jsx
@@ -2,7 +2,7 @@
 
 const Explore = () => {
   return (
-    <div>Explore</div>
+    <div>Stay ahead of the curve with our Dynamic Trend Analysis feature. Unlock the full potential of your tech stack with Horizon's intuitive and Stay ahead of the curve with our Dynamic Trend Analysis feature. Unlock the full potential of your tech stack with Horizon's intuitive and</div>
   )
 }
 
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,19 +1,19 @@
 import Button from "../components/Button";
 import arrowright from "../assets/arrowright.svg"
-
+import Ipad from "../assets/Ipad.svg"
 const Hero = () => {
   return (
-   <section id="Hero" className=" mt-20 ml-10 w-full flex  flex-col justify-center max-container">
-    <div  className="flex flex-col items-start">
+   <section id="Hero" className=" w-full flex xl:flex-row flex-col justify-center   ">
+    <div  className=" mt-10 ml-10 relative  flex flex-col justify-center items-start w-full ">
       <h1 className="text-4xl font-bold text-orange-500 ">Transform  Ideas </h1>
       <h1 className="text-4xl font-bold text-black-500 ">into apps with AI </h1>
       <p className="mt-10 font-bold"> The quick brown fox jumps over the lazy dog. Lorem ipsum  <br />dolor sit amet, consectetur adipiscing eli   <br /> </p>
-      <div>
-      <Button label="Get started " iconURL={arrowright}/>
-      </div>
-    </div>
-    <div>
 
+      <Button label="Get started " iconURL={arrowright}/>
+      
+    </div>
+    <div className ="  mt-10 mr-0 ml-96 flex justify-start items-start flex-wrap w-full ">
+<img src={Ipad} alt="Ipad" height={620} width={600} />
     </div>
    </section>
   )
